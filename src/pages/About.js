4e57b7fc02837a845c010ebb1a7bf66116b898c7
NadaHab <91700494+NadaHab@users.noreply.github.com>
2022-12-05 13:10:19 -0500

    About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,125 @@
-import * as React from 'react';
-import './About.css';
+import React from 'react'
+import styled from 'styled-components'
+import PageHero from '../components/PageHero'
 
-function About() {
+import "./About.css"
+import aboutImg from '../assets/hero-bcg.jpeg'
+import missionImg from '../assets/missionPhoto.jpg'
+import historyImg from '../assets/historyPhoto.jpg'
+const AboutPage = () => {
   return (
-    <div>
-      About
-    </div>
-
-  );
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'> {/* needs wrapper instead of div*/}
+        <img src={aboutImg} alt='nice desk' /> 
+        <article>
+          <div className='title'>
+            <h2>Our Story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+            accusantium sapiente tempora sed dolore esse deserunt eaque
+            excepturi, delectus error accusamus vel eligendi, omnis beatae.
+            Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
+            dolore, obcaecati incidunt sequi blanditiis est exercitationem
+            molestiae delectus saepe odio eligendi modi porro eaque in libero
+            minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
+            ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
+            similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
+            iste.
+          </p>
+        </article>
+      </Wrapper>
+      <Wrapper className='page section section-center'> {/* needs wrapper instead of div*/}
+        <img src={missionImg} alt='our mission' /> 
+        <article>
+          <div className='title'>
+            <h2>Mission</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+            accusantium sapiente tempora sed dolore esse deserunt eaque
+            excepturi, delectus error accusamus vel eligendi, omnis beatae.
+            Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
+            dolore, obcaecati incidunt sequi blanditiis est exercitationem
+            molestiae delectus saepe odio eligendi modi porro eaque in libero
+            minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
+            ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
+            similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
+            iste.
+          </p>
+        </article>
+      </Wrapper>
+      <Wrapper className='page section section-center'> {/* needs wrapper instead of div*/}
+        <img src={historyImg} alt='history phot' /> 
+        <article>
+          <div className='title'>
+            <h2>History</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+            accusantium sapiente tempora sed dolore esse deserunt eaque
+            excepturi, delectus error accusamus vel eligendi, omnis beatae.
+            Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
+            dolore, obcaecati incidunt sequi blanditiis est exercitationem
+            molestiae delectus saepe odio eligendi modi porro eaque in libero
+            minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
+            ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
+            similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
+            iste.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
 
-export default About;
+export default AboutPage
+
+
+
+
+// import * as React from 'react';
+// import './About.css';
+
+// function About() {
+//   return (
+//     <div>
+//       About
+//     </div>
+
+//   );
+// }
+
+// export default About;
