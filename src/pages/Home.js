d9c00974fca163f82fdb4d818ca959dcc69966d7
kim <kim2ee@hotmail.com>
2022-12-04 22:34:40 -0500

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.css";
+
+import Button from '@mui/material/Button';
+
+function Home() {
+  const go = (url) => {
+    
+  }
+
+     return (
+      <div>
+         <Button onClick={go}>
+                change color
+              </Button>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
