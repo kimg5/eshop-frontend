c603450f270cc6039e8638ed319ba2689609c21e
NadaHab <91700494+NadaHab@users.noreply.github.com>
2022-12-06 14:39:15 -0500

    admin dashboard fixed bootstrap

--- src/components/admin/UserList.js
+++ src/components/admin/UserList.js
@@ -1,10 +1,15 @@
+//npm install @mui/material @emotion/react @emotion/styled
+// npm install @mui/x-data-grid
+//npm i bootstrap
+
 import React, { useState, useEffect } from "react";
 //import axios from "axios";
 import { Table, Button } from "react-bootstrap";
-
-
+//import { Grid } from "@mui/x-data-grid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 export default function UserList(props) {
-    console.log(props);
+  console.log(props);
   /* LIST USERS */
   //const [usersList, setUsersList] = useState([]);
   var i = 0;
@@ -65,21 +70,22 @@ export default function UserList(props) {
       <h4 className="text-center display">Admin Dashboard</h4>
 
       <Table className="table table-striped table-hover align-middle table-sm caption-top table-responsive">
+        {/*<Table striped bordered hover>*/}
         <caption>List of Users</caption>
 
         <thead className="table-info text-center">
           <tr>
-            <th>#</th>
-            <th>UserName</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Actions</th>
+            <th scope="col">#</th>
+            <th scope="col">UserName</th>
+            <th scope="col">Email</th>
+            <th scope="col">Role</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
-        <tbody className=" text-center">
+        <tbody>
           {userList.map((user, i) => (
             <tr key={user._id}>
-              <td>{++i}</td>
+              <th scope="row">{++i}</th>
 
               <td>{user.username}</td>
               <td>{user.email}</td>
