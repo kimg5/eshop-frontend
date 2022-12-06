15ac68130dbfd8aac1929a9b170c78d5052a6813
NadaHab <91700494+NadaHab@users.noreply.github.com>
2022-12-06 10:55:39 -0500

    admin more work

--- src/components/admin/UserList.js
+++ src/components/admin/UserList.js
@@ -1,58 +1,62 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+//import axios from "axios";
 import { Table, Button } from "react-bootstrap";
-import "./style.css";
 
-export default function UserList() {
+
+export default function UserList(props) {
+    console.log(props);
   /* LIST USERS */
-  const [usersList, setUsersList] = useState([]);
+  //const [usersList, setUsersList] = useState([]);
   var i = 0;
+  const userList = props.rows;
 
   const handleStatusChange = (user) => {
-    try {
-      axios.put(
-        `${process.env.REACT_APP_BACKEND_URL}/updateStatus/` + user._id,
-        {
-          status: user.status,
-        }
-      )
-      .then(response =>{ 
-        getUserList();
-        console.log(response.data);
-      })
-    } catch (error) {
-      console.log(error.response.data.message);
-    }
+    console.log("status changeTheme");
+    // try {
+    //   axios.put(
+    //     `${process.env.REACT_APP_BACKEND_URL}/updateStatus/` + user._id,
+    //     {
+    //       status: user.status,
+    //     }
+    //   )
+    //   .then(response =>{
+    //     getUserList();
+    //     console.log(response.data);
+    //   })
+    // } catch (error) {
+    //   console.log(error.response.data.message);
+    // }
   };
 
   const handleDeleteClick = async (event, user) => {
     event.preventDefault();
-    try {
-      const { data } = await axios.delete(
-        `${process.env.REACT_APP_BACKEND_URL}/deleteUser/` + user._id
-      );
-      getUserList();
-      console.log(data);
-    } catch (error) {
-      console.log(error.response.data.message);
-    }
+    console.log("delete");
+    // try {
+    //   const { data } = await axios.delete(
+    //     `${process.env.REACT_APP_BACKEND_URL}/deleteUser/` + user._id
+    //   );
+    //   getUserList();
+    //   console.log(data);
+    // } catch (error) {
+    //   console.log(error.response.data.message);
+    // }
   };
 
-  const getUserList = async () => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/fetchUsers`)
-      .then((response) => {
-        setUsersList(response.data);
-      });
-  }
+  //   const getUserList = async () => {
+  //     axios
+  //       .get(`${process.env.REACT_APP_BACKEND_URL}/fetchUsers`)
+  //       .then((response) => {
+  //         setUsersList(response.data);
+  //       });
+  //   }
 
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/fetchUsers`)
-      .then((response) => {
-        setUsersList(response.data);
-      });
-  }, []);
+  //   useEffect(() => {
+  //     axios
+  //       .get(`${process.env.REACT_APP_BACKEND_URL}/fetchUsers`)
+  //       .then((response) => {
+  //         setUsersList(response.data);
+  //       });
+  //   }, []);
 
   return (
     <div className="container">
@@ -65,7 +69,7 @@ export default function UserList() {
 
         <thead className="table-info text-center">
           <tr>
-            <th>#</th>   
+            <th>#</th>
             <th>UserName</th>
             <th>Email</th>
             <th>Role</th>
@@ -73,10 +77,10 @@ export default function UserList() {
           </tr>
         </thead>
         <tbody className=" text-center">
-          {usersList.map((user, i) => (
+          {userList.map((user, i) => (
             <tr key={user._id}>
               <td>{++i}</td>
-              
+
               <td>{user.username}</td>
               <td>{user.email}</td>
               <td>{user.role}</td>
--- src/pages/adminDashboard/Admin.js
+++ src/pages/adminDashboard/Admin.js
@@ -1,17 +1,22 @@
 //import { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
 import UserList from "../../components/admin/UserList";
 import "./Admin.css";
 
 export default function Admin() {
-  const { user } = useSelector((state) => ({ ...state }));
+  // const { user } = useSelector((state) => ({ ...state }));
+  const userList = [
+    { username: "john", email: "123@", role: "user" },
+    { username: "micheal", email: "123", role: "user" },
+  ];
 
   return (
     <>
       {/* <Header page="home" /> */}
       <div className="userList">
-        <UserList />
+        <UserList rows={userList} />{" "}
+        {/* define userlist in another component and import it */}
       </div>
     </>
   );
