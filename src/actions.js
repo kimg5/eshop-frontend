7141290db8850706e4c1e54ae544b24dbab26b42
nanoiris <y.ding@knights.ucf.edu>
2022-12-05 21:18:11 -0500

    cart page & related still in progress

--- src/actions.js
+++ src/actions.js
@@ -3,3 +3,9 @@ export const POST_LOGIN_SUCCESS = 'POST_LOGIN_SUCCESS'
 export const POST_LOGIN_ERROR = 'POST_LOGIN_ERROR'
 
 export const LOGOUT = 'LOGOUT'
+// cart related 
+export const ADD_TO_CART = 'ADD_TO_CART'
+export const REMOVE_CART_ITEM = 'REMOVE_CART_ITEM'
+export const TOGGLE_CART_ITEM_AMOUNT = 'TOGGLE_CART_ITEM_AMOUNT'
+export const CLEAR_CART = 'CLEAR_CART'
+export const COUNT_CART_TOTALS = 'COUNT_CART_TOTALS'
---
+++ src/components/CardContent.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useCartContext } from '../context/cartContext'
+import { Link } from 'react-router-dom'
+import CartColumns from './CartColumns'
+import CartItem from './CartItem'
+import CartTotals from './CartTotals'
+export const CartContent = () => {
+  const { cart, clearCart } = useCartContext()
+
+  return (
+    <div className='section section-center'>
+      <CartColumns />
+      {cart.map((item) => {
+        return <CartItem key={item.id} {...item} />
+      })}
+      <hr />
+      <div className='link-container'>
+        <Link to='/products' className='link-btn'>
+          continue shopping
+        </Link>
+        <button
+          type='button'
+          className='link-btn clear-btn'
+          onClick={clearCart}
+        >
+          clear shopping cart
+        </button>
+      </div>
+      <CartTotals />
+    </div>
+  )
+}
