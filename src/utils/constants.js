8bfe9ba21163d8fa90df4036c02aa2b9fb55491d
NadaHab <91700494+NadaHab@users.noreply.github.com>
2022-12-06 21:38:36 -0500

    Single Product page

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,6 @@ export const api_url = 'http://localhost:3001'
 export const api_login_url = api_url + '/auth/login';
 export const api_products_url = api_url + '/products';
 
+export const products_url = 'https://course-api.com/react-store-products';
+
+export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
