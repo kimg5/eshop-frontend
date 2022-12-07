78987b39527ff15ba24662a030ae4e9f0679454d
kim <kim2ee@hotmail.com>
2022-12-07 10:57:25 -0500

    add filter and search url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,9 @@ export const api_url = 'http://localhost:3001'
 export const api_login_url = api_url + '/auth/login';
 export const api_products_url = api_url + '/products';
 
+export const api_filters_url = api_url + '/filters';
+export const api_search_url = api_url + "/products/search";
+
 export const products_url = 'https://course-api.com/react-store-products';
 
 export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
