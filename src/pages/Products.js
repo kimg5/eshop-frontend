d0a04d2622a7a5e7205713cdb876500a29f9fa42
kim <kim2ee@hotmail.com>
2022-12-08 14:48:09 -0500

    change the prodcts page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,36 +2,37 @@ import * as React from "react";
 import Grid from "@mui/material/Unstable_Grid2";
 import { Divider } from "@mui/material";
 
+import Box from "@mui/material/Box";
 import Filters from "./Filters";
 import GridView from "../components/GridView";
 import SortBy from "../components/SortBy";
 import http from "../utils/http";
 import { api_search_url, api_products_detail_url } from "../utils/constants";
-import ProductDetail from './ProductDetail'
+import ProductDetail from "./ProductDetail";
 
 function Products() {
-  const [detail, setDetail] = React.useState({isOpen:false,row:{}});
-  
+  const [detail, setDetail] = React.useState({ isOpen: false, row: {} });
+
   const detailHandler = (row) => {
     console.log("gotDetail");
     http.get(api_products_detail_url + row._id).then((resp) => {
-      setDetail({isOpen:true,row:resp});
+      setDetail({ isOpen: true, row: resp });
     });
   };
 
   const detailCloseHandler = (row) => {
-    setDetail({isOpen:false,row:row});
-  }  
+    setDetail({ isOpen: false, row: row });
+  };
 
   return (
     <>
-      <ProductDetail detail={detail} closeHandler={detailCloseHandler}/>
-      <ProductList detailHandler={detailHandler}/>
+      <ProductDetail detail={detail} closeHandler={detailCloseHandler} />
+      <ProductList detailHandler={detailHandler} />
     </>
   );
 }
 
-function ProductList({detailHandler}) {
+function ProductList({ detailHandler }) {
   const initialState = {
     isLoading: false,
     products: [],
@@ -51,21 +52,31 @@ function ProductList({detailHandler}) {
   };
 
   return (
-    <Grid container spacing={2} width={"90vw"}>
-      <Grid item xs={2}>
-        <Filters filterHandler={filterHandler} />
+    <Box
+    sx={{
+      marginTop: 8,
+      marginLeft: -15,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    }}
+  >
+      <Grid container spacing={2} width={"90vw"}>
+        <Grid item xs={2}>
+          <Filters filterHandler={filterHandler} />
+        </Grid>
+        <Grid item xs={10} sx={{ pl: 6 }}>
+          <Divider textAlign="right">
+            <SortBy handler={sortHandler} />
+          </Divider>
+          {state.products ? (
+            <GridView detailHandler={detailHandler} style={{ space: 2, columns: 3, imageHeight: 180 }} rows={state.products} />
+          ) : (
+            <div>No products</div>
+          )}
+        </Grid>
       </Grid>
-      <Grid item xs={10} sx={{ pl: 6 }}>
-        <Divider textAlign="right">
-          <SortBy handler={sortHandler} />
-        </Divider>
-        {state.products ? (
-          <GridView detailHandler={detailHandler} style={{ space: 2, columns: 3, imageHeight: 180 }} rows={state.products} />
-        ) : (
-          <div>No products</div>
-        )}
-      </Grid>
-    </Grid>
+   </Box>
   );
 }
 
